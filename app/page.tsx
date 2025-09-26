00fa6ba9104fd1d8d8cebc21fd5b7a25fbaf60ea
54-star <onuoraruth608@gmail.com>
2025-09-26 22:12:54 +0100

    feat: updated homepage heading to 'Welcome to Flowsate Portfolio'

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold ">Home Page</h1>
+      <h1 className="text-3xl font-bold ">Welcome to Flowsate Portfolio</h1>
     </main>
   );
 }
