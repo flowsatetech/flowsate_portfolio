02aca4425c895254ddfabef0e265742eb990f161
Sifan23 <sifanyg123@gmail.com>
2025-10-10 11:41:48 +0300

    feat: added clients section

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,3 +1,5 @@
+import Clients from '@/components/clients'
+import ComprehensiveServices from '@/components/comprehensive-services'
 import EnterpriseSolutions from '@/components/enterprise-solutions'
 import Stats from '@/components/stats'
 import React from 'react'
@@ -7,6 +9,8 @@ const Services = () => {
     <div className='flex flex-col items-center justify-center '>
       <EnterpriseSolutions />
       <Stats />
+      {/* <ComprehensiveServices /> */}
+      <Clients />
       </div>
   )
 }
