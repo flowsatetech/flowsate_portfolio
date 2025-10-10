626998a196d33ba4b1ddfd770ed891b37b503661
Sifan23 <sifanyg123@gmail.com>
2025-10-10 20:59:53 +0300

    feat: service card constants

--- constants/service-card.ts
+++ constants/service-card.ts
@@ -1,24 +1,132 @@
 export const serviceCard = [
-    {
-        backgrounndImage: '/images/ui-card-back.png',
-        
-    },
-    {
-        backgrounndImage: '/images/web-card-back.png'
-    },
-    {
-        backgrounndImage: '/images/cloud-card-back.png'
-    },
-    {
-        backgrounndImage: '/images/mobile-card-back.png'
-    },
-    {
-        backgrounndImage: '/images/brand-card-back.png'
-    },
-    {
-        backgrounndImage: '/images/data-card-back.png'
-    },
-    {
-        backgrounndImage: '/images/Ai-card-back.png'
-    }
-]
+  {
+    backgrounndImage: "/images/ui-card-back.png",
+    type: [
+      {
+        image: "/images/arrow.png",
+        title: "User Research",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Prototyping",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Design System",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Usability Testing",
+      },
+    ],
+    description:
+      "User-centered design solutions that combines aesthetic excellence with functional usability",
+    timeline: "4-8 weeks",
+    complexity: "Medium",
+    technologies: ["Figma", "Adobe", "Sketch", "+3more"],
+  },
+{
+    backgrounndImage: "/images/ui-card-back.png",
+    type: [
+      {
+        image: "/images/arrow.png",
+        title: "User Research",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Prototyping",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Design System",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Usability Testing",
+      },
+    ],
+    description:
+      "User-centered design solutions that combines aesthetic excellence with functional usability",
+    timeline: "4-8 weeks",
+    complexity: "Medium",
+    technologies: ["Figma", "Adobe", "Sketch", "+3more"],
+  },
+  {
+    backgrounndImage: "/images/ui-card-back.png",
+    type: [
+      {
+        image: "/images/arrow.png",
+        title: "User Research",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Prototyping",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Design System",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Usability Testing",
+      },
+    ],
+    description:
+      "User-centered design solutions that combines aesthetic excellence with functional usability",
+    timeline: "4-8 weeks",
+    complexity: "Medium",
+    technologies: ["Figma", "Adobe", "Sketch", "+3more"],
+  },
+  {
+    backgrounndImage: "/images/ui-card-back.png",
+    type: [
+      {
+        image: "/images/arrow.png",
+        title: "User Research",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Prototyping",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Design System",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Usability Testing",
+      },
+    ],
+    description:
+      "User-centered design solutions that combines aesthetic excellence with functional usability",
+    timeline: "4-8 weeks",
+    complexity: "Medium",
+    technologies: ["Figma", "Adobe", "Sketch", "+3more"],
+  },
+  {
+    backgrounndImage: "/images/ui-card-back.png",
+    type: [
+      {
+        image: "/images/arrow.png",
+        title: "User Research",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Prototyping",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Design System",
+      },
+      {
+        image: "/images/arrow.png",
+        title: "Usability Testing",
+      },
+    ],
+    description:
+      "User-centered design solutions that combines aesthetic excellence with functional usability",
+    timeline: "4-8 weeks",
+    complexity: "Medium",
+    technologies: ["Figma", "Adobe", "Sketch", "+3more"],
+  },
+];
