5db845eee3345363a20bc9e341e292dfb1e1dac6
Sifan23 <sifanyg123@gmail.com>
2025-10-15 10:36:05 +0300

    feat: importing home and background slider

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,12 @@
+import BackgroundSlider from "@/components/background-slider";
 import HomeSlider from "@/components/home-slider";
 
 export default function Test() {
 
   return (
-    <><HomeSlider/></>
+    <>
+    <HomeSlider/>
+    <BackgroundSlider/>
+    </>
   );
 }
