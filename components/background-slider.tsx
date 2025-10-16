2700f7eaed554bd46d951afa472f13fb87a8c5c9
Sifan23 <sifanyg123@gmail.com>
2025-10-16 06:50:09 +0300

    feat: background slider component

--- components/background-slider.tsx
+++ components/background-slider.tsx
@@ -23,45 +23,53 @@ export default function BackgroundSlider() {
       >
         <SwiperSlide>
           <Image
-            src="/images/Rectangle1.png" // Replace with your image path
+            src="/images/Rectangle1.png" 
             alt="Slide 1" 
             className="slide-background"
             fill
           />
-          <div className="slide-content">Slide 1</div>
+          <div className="slide-content">We Provide Web/Mobile Design Solutions</div>
         </SwiperSlide>
         
         <SwiperSlide>
           <Image
-            src="/images/frame2.png" // Replace with your image path
+            src="/images/software-development.png" 
             alt="Slide 2" 
             className="slide-background"
             fill
           />
-          <div className="slide-content">Slide 2</div>
+          <div className="slide-content">Software Development</div>
         </SwiperSlide>
-        
-        <SwiperSlide>
+
+         <SwiperSlide>
           <Image
-            src="/images/frame3.png" // Replace with your image path
+            src="/images/production.png" 
             alt="Slide 3" 
             className="slide-background"
             fill
           />
-          <div className="slide-content">Slide 3</div>
+          <div className="slide-content">Production</div>
         </SwiperSlide>
         
         <SwiperSlide>
-          <Image 
-            src="/images/frame1.png" // Replace with your image path
+          <Image
+            src="/images/SEO.png" 
             alt="Slide 4" 
             className="slide-background"
             fill
           />
-          <div className="slide-content">Slide 4</div>
+          <div className="slide-content">SEO</div>
         </SwiperSlide>
         
-        {/* Add more slides as needed */}
+        <SwiperSlide>
+          <Image 
+            src="/images/consultancy-service.png" 
+            alt="Slide 5" 
+            className="slide-background"
+            fill
+          />
+          <div className="slide-content">Consultancy Service</div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
